31e8253c7080998d5e6a3a3d22f534efeaa709f8
Sara R Mills <SaraRMills@gmail.com>
2020-05-04 12:38:59 -0400

    second drill complete

--- practice.js
+++ practice.js
@@ -1,12 +1,30 @@
 
-
 function sheep(num) {
   if (num === 0) {
     console.log("All sheep jumped over the fence")
-    return "All sheep jumped over the fence"}
+    return "All sheep jumped over the fence"
+  }
 
   console.log(`${num}: Another sheep jumps over the fence`) 
   return sheep(num-1) 
 }
 
 sheep(3)
+
+
+function powerCalculator(base, exp) {
+  if (exp <= 0) {
+    return "exponent should be >= 0"
+  }
+  else if (base <= 0) {
+    return "base should be >= 0"
+  }
+  else if (exp === 1) {
+    return base
+  }
+  exp--;
+  return base * powerCalculator(base, exp)
+  }
+
+powerCalculator(10,4)
+powerCalculator(10,2)
