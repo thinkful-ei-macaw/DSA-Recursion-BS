bb0cc1c2ee37272238a3ac628e136474f7af2beb
Sara R Mills <SaraRMills@gmail.com>
2020-05-04 15:44:36 -0400

    7th drill

--- practice.js
+++ practice.js
@@ -137,3 +137,22 @@ fib(7)
 
 
 Write a recursive function that finds the factorial of a given number. The factorial of a number can be found by multiplying that number by each number between itself and 1. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+input: 5
+output: 120
+
+// 5 *4 
+// 5* 3
+// 5* 2
+// 5* 1
+// sum of above
+function fact(num) {
+  if (num === 1){
+    return 1
+  }
+  
+  return num * fact(num - 1)
+}
+
+fact(4)
+
+
