16990dc98f5a2e325e2271e54d57e941619237ac
Sara R Mills <SaraRMills@gmail.com>
2020-05-04 14:51:14 -0400

    drill 4 done

--- practice.js
+++ practice.js
@@ -50,3 +50,40 @@ function reverseString(string, newS='') {
 }
 
 reverseString('string')
+
+
+// Calculate the nth triangular number.
+//   A triangular number counts the objects that can form an equilateral triangle.
+//   The nth triangular number is the number of dots composing a triangle with n dots on a side, and is equal to the sum of the n natural numbers from 1 to n.
+//   This is the Triangular Number Sequence: 1, 3, 6, 10, 15, 21, 28, 36, 45.
+  
+//                                           *
+//                           *             *   *
+//             *           *    *        *   *   *
+// *     |   *   *  |   *    *    *  | *   *   *   *
+
+//  1st       2nd           3rd             nth?  
+
+// output total number of dots
+// input num of dots on a side
+
+// in  out
+// 1   1
+// 2    3
+// 3   6
+// 4   10
+// 5   15
+// 6    21
+// 7    28
+
+function triangle(num) {
+  if (num === 1) {
+    return 1
+  }
+
+  return num + triangle(num - 1)
+}
+
+triangle(3)
+
+
