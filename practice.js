ecb43dfde5b22a679eb719f0eadc1ff22db7563f
Sara R Mills <SaraRMills@gmail.com>
2020-05-04 16:59:42 -0400

    working on maze

--- practice.js
+++ practice.js
@@ -1,158 +1,223 @@
 
-function sheep(num) {
-  if (num === 0) {
-    console.log("All sheep jumped over the fence")
-    return "All sheep jumped over the fence"
-  }
+// function sheep(num) {
+//   if (num === 0) {
+//     console.log("All sheep jumped over the fence")
+//     return "All sheep jumped over the fence"
+//   }
 
-  console.log(`${num}: Another sheep jumps over the fence`) 
-  return sheep(num-1) 
-}
+//   console.log(`${num}: Another sheep jumps over the fence`) 
+//   return sheep(num-1) 
+// }
 
-sheep(3)
-
-
-function powerCalculator(base, exp) {
-  if (exp <= 0) {
-    return "exponent should be >= 0"
-  }
-  else if (base <= 0) {
-    return "base should be >= 0"
-  }
-  else if (exp === 1) {
-    return base
-  }
-  exp--;
-  return base * powerCalculator(base, exp)
-  }
-
-powerCalculator(10,4)
-powerCalculator(10,2)
-
-// input: string
-// output: gnirts
-
-// string
-// tring  s
-// ring   ts
-// ing    rts
-// ng     irts
-// g      girts
-//        gnirts
-function reverseString(string, newS='') {
-  if  (!string.length) {
-    return ''
-  }
-  let char = string.slice(string.length-1)
-  newS = newS + char
-  string = string.slice(0, -1)
-  return newS + reverseString(string, newS)
-}
-
-reverseString('string')
+// sheep(3)
 
 
-// Calculate the nth triangular number.
-//   A triangular number counts the objects that can form an equilateral triangle.
-//   The nth triangular number is the number of dots composing a triangle with n dots on a side, and is equal to the sum of the n natural numbers from 1 to n.
-//   This is the Triangular Number Sequence: 1, 3, 6, 10, 15, 21, 28, 36, 45.
+// function powerCalculator(base, exp) {
+//   if (exp <= 0) {
+//     return "exponent should be >= 0"
+//   }
+//   else if (base <= 0) {
+//     return "base should be >= 0"
+//   }
+//   else if (exp === 1) {
+//     return base
+//   }
+//   exp--;
+//   return base * powerCalculator(base, exp)
+//   }
+
+// powerCalculator(10,4)
+// powerCalculator(10,2)
+
+// // input: string
+// // output: gnirts
+
+// // string
+// // tring  s
+// // ring   ts
+// // ing    rts
+// // ng     irts
+// // g      girts
+// //        gnirts
+// function reverseString(string, newS='') {
+//   if  (!string.length) {
+//     return ''
+//   }
+//   let char = string.slice(string.length-1)
+//   newS = newS + char
+//   string = string.slice(0, -1)
+//   return newS + reverseString(string, newS)
+// }
+
+// reverseString('string')
+
+
+// // Calculate the nth triangular number.
+// //   A triangular number counts the objects that can form an equilateral triangle.
+// //   The nth triangular number is the number of dots composing a triangle with n dots on a side, and is equal to the sum of the n natural numbers from 1 to n.
+// //   This is the Triangular Number Sequence: 1, 3, 6, 10, 15, 21, 28, 36, 45.
   
-//                                           *
-//                           *             *   *
-//             *           *    *        *   *   *
-// *     |   *   *  |   *    *    *  | *   *   *   *
+// //                                           *
+// //                           *             *   *
+// //             *           *    *        *   *   *
+// // *     |   *   *  |   *    *    *  | *   *   *   *
 
-//  1st       2nd           3rd             nth?  
+// //  1st       2nd           3rd             nth?  
 
-// output total number of dots
-// input num of dots on a side
+// // output total number of dots
+// // input num of dots on a side
 
-// in  out
-// 1   1
-// 2    3
-// 3   6
-// 4   10
-// 5   15
-// 6    21
-// 7    28
+// // in  out
+// // 1   1
+// // 2    3
+// // 3   6
+// // 4   10
+// // 5   15
+// // 6    21
+// // 7    28
 
-function triangle(num) {
-  if (num === 1) {
-    return 1
+// function triangle(num) {
+//   if (num === 1) {
+//     return 1
+//   }
+
+//   return num + triangle(num - 1)
+// }
+
+// triangle(3)
+
+
+// // Write a recursive function that splits a string based on a separator (similar to String.prototype.split). Don't use JS array's split function to solve this problem.
+
+// // Input: 02/20/2020
+// // Output: ["02", "20", "2020"]
+
+// // 02/20/2020  
+// // 20/2020      ['02']
+// // 2020          ['02','20']
+// //             ["02", "20", "2020"]
+
+// function splitter(str, separator, test = 0) {
+
+//   if (!str.includes(separator)){
+//     return [str]
+//   }
+
+//   let index = str.search(separator);
+//   let newS = str.slice(0, index)
+//   str = str.slice(index + 1, str.length)
+//   console.log(str)
+//   return [newS , ...splitter(str, separator)]
+// }
+
+// splitter('02/20/2020', '/')
+
+
+// // Write a recursive function that prints the Fibonacci sequence of a given number. The Fibonacci sequence is a series of numbers in which each number is the sum of the 2 preceding numbers. For example, the 7th Fibonacci number in a Fibonacci sequence is 13. The sequence looks as follows: 1, 1, 2, 3, 5, 8, 13.
+
+// // 1 1
+// // 1 2
+// // 2 3
+// // 3 4
+// // 5 5
+// // 8 6
+// // 13 7
+
+// function fib(num) {
+//   if (num === 1) {
+//     return 1
+//   }
+//   if (num === 2) {
+//     return 1
+//   }
+//   return fib(num - 1)  + fib(num - 2)
+// }
+
+// fib(7)
+
+
+// Write a recursive function that finds the factorial of a given number. The factorial of a number can be found by multiplying that number by each number between itself and 1. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+// input: 5
+// output: 120
+
+// // 5 *4 
+// // 5* 3
+// // 5* 2
+// // 5* 1
+// // sum of above
+// function fact(num) {
+//   if (num === 1){
+//     return 1
+//   }
+  
+//   return num * fact(num - 1)
+// }
+
+// fact(4)
+
+
+
+let mySmallMaze = [
+    [' ', ' ', ' '],
+    [' ', '*', ' '],
+    [' ', ' ', 'e']
+];
+
+let maze = [
+    [' ', ' ', ' ', '*', ' ', ' ', ' '],
+    ['*', '*', ' ', '*', ' ', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', '*', '*', '*', '*', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+];
+
+//check value of each item in array for ' '
+//if open path is found, increment index2 and return r
+//if *, increment index1 (x)
+//if " ", increment index2 (y) and return d
+//if "e", return
+
+function travelMaze(maze, row = 0, column = 0, path, dir, position = 0) {
+  // console.log(row, column)
+  if (row < 0 || column < 0) {
+    return;
   }
 
-  return num + triangle(num - 1)
-}
-
-triangle(3)
-
-
-// Write a recursive function that splits a string based on a separator (similar to String.prototype.split). Don't use JS array's split function to solve this problem.
-
-// Input: 02/20/2020
-// Output: ["02", "20", "2020"]
-
-// 02/20/2020  
-// 20/2020      ['02']
-// 2020          ['02','20']
-//             ["02", "20", "2020"]
-
-function splitter(str, separator, test = 0) {
-
-  if (!str.includes(separator)){
-    return [str]
+  if (row >= maze.length || column >= maze[0].length) {
+    return;
   }
 
-  let index = str.search(separator);
-  let newS = str.slice(0, index)
-  str = str.slice(index + 1, str.length)
-  console.log(str)
-  return [newS , ...splitter(str, separator)]
-}
+  path[position] = dir
+  position++
+  // console.log(maze, row, column)
 
-splitter('02/20/2020', '/')
-
-
-// Write a recursive function that prints the Fibonacci sequence of a given number. The Fibonacci sequence is a series of numbers in which each number is the sum of the 2 preceding numbers. For example, the 7th Fibonacci number in a Fibonacci sequence is 13. The sequence looks as follows: 1, 1, 2, 3, 5, 8, 13.
-
-// 1 1
-// 1 2
-// 2 3
-// 3 4
-// 5 5
-// 8 6
-// 13 7
-
-function fib(num) {
-  if (num === 1) {
-    return 1
-  }
-  if (num === 2) {
-    return 1
-  }
-  return fib(num - 1)  + fib(num - 2)
-}
-
-fib(7)
-
-
-Write a recursive function that finds the factorial of a given number. The factorial of a number can be found by multiplying that number by each number between itself and 1. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
-input: 5
-output: 120
-
-// 5 *4 
-// 5* 3
-// 5* 2
-// 5* 1
-// sum of above
-function fact(num) {
-  if (num === 1){
-    return 1
+  if (maze[row][column] === 'e') {
+    console.log(path)
+    return;
   }
   
-  return num * fact(num - 1)
+  if (maze[row][column] === ' ') {
+    maze[row][column] = 'visted';
+    
+    travelMaze(maze, row, column + 1, path, 'R') //right
+    travelMaze(maze, row + 1, column, path, 'D') //down
+    travelMaze(maze, row - 1, column, path, 'U') //up
+    travelMaze(maze, row, column - 1, path, 'L') //left
+  }
+  position--
 }
 
-fact(4)
+travelMaze(maze, 0, 0, [], '')
 
 
+// An anagram is any word or phrase that uses the letters of a given ("subject") word or phrase in another, rearranged order. Write a function that creates an anagram list, listing all the rearrangements of a given word. For example, if the user types "east", the program should list all 24 permutations, including "eats", "etas", "teas", and non-words like "tsae".
+
+// Hint: For your algorithm, you might want to think about a prefix and use that to create the new words. For example, given "east", use "e" as a prefix and place it in front of all 6 permutations of "ast" — "ast", "ats", "sat", "sta", "tas", and "tsa". This will give you the words "east", "eats", "esat", "esta", "etas", and "etsa". Continue this way until you find all the anagrams for "east". Then you can use "a" as a prefix and permute the remaining words "est". For "east", there should be 24 words.
+
+
+
+// function anagram(word) {
+  
+// }
+
+// anagram('east')
