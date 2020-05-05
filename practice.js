ce801f80d2d9ba35029eb9f9bb1ef4c94037fa27
Brannen Petit <bpetit940@gmail.com>
2020-05-05 11:21:10 -0500

    iterated 1st drill

--- practice.js
+++ practice.js
@@ -1,16 +1,26 @@
-
 // function sheep(num) {
 //   if (num === 0) {
 //     console.log("All sheep jumped over the fence")
 //     return "All sheep jumped over the fence"
 //   }
 
-//   console.log(`${num}: Another sheep jumps over the fence`) 
-//   return sheep(num-1) 
+//   console.log(`${num}: Another sheep jumps over the fence`)
+//   return sheep(num-1)
 // }
 
 // sheep(3)
 
+function sheep(num) {
+  for (let i = num; i > 0; i--) {
+    console.log(`${i}: Another sheep jumps over teh fence`);
+    if (i === 1) {
+      console.log("All sheep jumped over the fence");
+      return "All sheep jumped over the fence";
+    }
+  }
+}
+
+sheep(3);
 
 // function powerCalculator(base, exp) {
 //   if (exp <= 0) {
@@ -51,18 +61,17 @@
 
 // reverseString('string')
 
-
 // // Calculate the nth triangular number.
 // //   A triangular number counts the objects that can form an equilateral triangle.
 // //   The nth triangular number is the number of dots composing a triangle with n dots on a side, and is equal to the sum of the n natural numbers from 1 to n.
 // //   This is the Triangular Number Sequence: 1, 3, 6, 10, 15, 21, 28, 36, 45.
-  
+
 // //                                           *
 // //                           *             *   *
 // //             *           *    *        *   *   *
 // // *     |   *   *  |   *    *    *  | *   *   *   *
 
-// //  1st       2nd           3rd             nth?  
+// //  1st       2nd           3rd             nth?
 
 // // output total number of dots
 // // input num of dots on a side
@@ -86,13 +95,12 @@
 
 // triangle(3)
 
-
 // // Write a recursive function that splits a string based on a separator (similar to String.prototype.split). Don't use JS array's split function to solve this problem.
 
 // // Input: 02/20/2020
 // // Output: ["02", "20", "2020"]
 
-// // 02/20/2020  
+// // 02/20/2020
 // // 20/2020      ['02']
 // // 2020          ['02','20']
 // //             ["02", "20", "2020"]
@@ -111,7 +119,6 @@
 // }
 
 // splitter('02/20/2020', '/')
-
 
 // // Write a recursive function that prints the Fibonacci sequence of a given number. The Fibonacci sequence is a series of numbers in which each number is the sum of the 2 preceding numbers. For example, the 7th Fibonacci number in a Fibonacci sequence is 13. The sequence looks as follows: 1, 1, 2, 3, 5, 8, 13.
 
@@ -135,12 +142,11 @@
 
 // fib(7)
 
-
 // Write a recursive function that finds the factorial of a given number. The factorial of a number can be found by multiplying that number by each number between itself and 1. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
 // input: 5
 // output: 120
 
-// // 5 *4 
+// // 5 *4
 // // 5* 3
 // // 5* 2
 // // 5* 1
@@ -149,26 +155,24 @@
 //   if (num === 1){
 //     return 1
 //   }
-  
+
 //   return num * fact(num - 1)
 // }
 
 // fact(4)
 
-
-
 let mySmallMaze = [
-    [' ', ' ', ' '],
-    [' ', '*', ' '],
-    [' ', ' ', 'e']
+  [" ", " ", " "],
+  [" ", "*", " "],
+  [" ", " ", "e"],
 ];
 
 let myBigMaze = [
-    [' ', ' ', ' ', '*', ' ', ' ', ' '],
-    ['*', '*', ' ', '*', ' ', '*', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', '*', '*', '*', '*', '*', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+  [" ", " ", " ", "*", " ", " ", " "],
+  ["*", "*", " ", "*", " ", "*", " "],
+  [" ", " ", " ", " ", " ", " ", " "],
+  [" ", "*", "*", "*", "*", "*", " "],
+  [" ", " ", " ", " ", " ", " ", "e"],
 ];
 
 //check value of each item in array for ' '
@@ -177,49 +181,46 @@ let myBigMaze = [
 //if " ", increment index2 (y) and return d
 //if "e", return
 
-function travelMaze(maze, row, column, path, dir, position) {
-  // console.log(row, column)
-  if (row < 0 || column < 0) {
-    return;
-  }
+// function travelMaze(maze, row, column, path, dir, position) {
+//   // console.log(row, column)
+//   if (row < 0 || column < 0) {
+//     return;
+//   }
 
-  if (row >= maze.length || column >= maze[0].length) {
-    return;
-  }
+//   if (row >= maze.length || column >= maze[0].length) {
+//     return;
+//   }
 
-  path[position] = dir
-  position++
-  // console.log(maze, row, column)
+//   path[position] = dir;
+//   position++;
+//   // console.log(maze, row, column)
 
-  if (maze[row][column] === 'e') {
-    console.log(path)
-    return;
-  }
-  
-  if (maze[row][column] === ' ') {
-    maze[row][column] = 'visted';
-    
-    travelMaze(maze, row, column + 1, path, 'R', position) //right
-    travelMaze(maze, row + 1, column, path, 'D', position) //down
-    travelMaze(maze, row - 1, column, path, 'U', position) //up
-    travelMaze(maze, row, column - 1, path, 'L', position) //left
+//   if (maze[row][column] === "e") {
+//     console.log(path);
+//     return;
+//   }
 
-    maze[row][column] = ' ';
-  }
-  position--
-}
+//   if (maze[row][column] === " ") {
+//     maze[row][column] = "visted";
 
-travelMaze(myBigMaze, 0, 0, [], 'S', 0)
+//     travelMaze(maze, row, column + 1, path, "R", position); //right
+//     travelMaze(maze, row + 1, column, path, "D", position); //down
+//     travelMaze(maze, row - 1, column, path, "U", position); //up
+//     travelMaze(maze, row, column - 1, path, "L", position); //left
 
+//     maze[row][column] = " ";
+//   }
+//   position--;
+// }
+
+// travelMaze(myBigMaze, 0, 0, [], "S", 0);
 
 // An anagram is any word or phrase that uses the letters of a given ("subject") word or phrase in another, rearranged order. Write a function that creates an anagram list, listing all the rearrangements of a given word. For example, if the user types "east", the program should list all 24 permutations, including "eats", "etas", "teas", and non-words like "tsae".
 
 // Hint: For your algorithm, you might want to think about a prefix and use that to create the new words. For example, given "east", use "e" as a prefix and place it in front of all 6 permutations of "ast" — "ast", "ats", "sat", "sta", "tas", and "tsa". This will give you the words "east", "eats", "esat", "esta", "etas", and "etsa". Continue this way until you find all the anagrams for "east". Then you can use "a" as a prefix and permute the remaining words "est". For "east", there should be 24 words.
 
-
-
 // function anagram(word) {
-  
+
 // }
 
 // anagram('east')
